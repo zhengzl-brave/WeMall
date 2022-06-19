b60162d9edc634fa2a5a2c7072074cbca921a969
zzl <361255525@qq.com>
2022-06-19 22:51:42 +0800

    feat(tabbar): 新增tabbar模块

---
+++ pages/mine/mine.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    Mine
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
