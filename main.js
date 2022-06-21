6f781f0fe61ac7eb23221d7d42a5c2e45b75b4fc
zzl <361255525@qq.com>
2022-06-21 22:39:49 +0800

    feat(首页): 新增首页轮播，添加分包配置

--- main.js
+++ main.js
@@ -5,6 +5,21 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+// 引入request请求封装的包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://www.uinav.com'
+// 请求拦截
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+// 响应拦截
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
 App.mpType = 'app'
 
 const app = new Vue({
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
   <view>
-    Home
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, index) in swiperList" :key="index">
+        <view class="swiper-item">
+          <image :src="item.image_src"></image>
+        </view>
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
@@ -8,12 +14,35 @@
   export default {
     data() {
       return {
-        
+        swiperList: []
       };
+    },
+    onLoad() {
+      this.getSwiperList()
+    },
+    methods: {
+      async getSwiperList() {
+        const { data:res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        if(res.meta.status !== 200) {
+          return uni.showToast({
+            title: '数据加载失败',
+            duration: 1500,
+            icon: 'none'
+          })
+        }
+        this.swiperList = res.message
+        console.log(res)
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+swiper {
+  height: 330rpx;
+  .swiper-item, image {
+    height: 100%;
+    width: 100%;
+  }
+}
 </style>
