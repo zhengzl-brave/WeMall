e55372701646df5368962b27ed1af607b90087ad
zzl <361255525@qq.com>
2022-06-26 15:14:48 +0800

    feat(商品列表): 新增商品列表，上拉加载下拉刷新

---
+++ components/goods-item/goods-item.vue
@@ -0,0 +1,87 @@
+<template>
+  <view>
+    <block v-for="(item, index) in goodsList" :key="index">
+      <view class="goods-item" @click="handleClcik(item)">
+        <view class="goods-left">
+          <image :src="item[optonObj.goods_small_logo] || defaultImg"></image>
+        </view>
+        <view class="goods-right">
+          <view class="goods-title">{{ item[optonObj.goods_name] || '' }}</view>
+          <view class="goods-info">
+            <view class="goods-price">￥{{ priceFormat(item[optonObj.goods_price]) }}</view>
+          </view>
+        </view>
+      </view>
+    </block>
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      goodsList: {
+        type: Array,
+        default: () => []
+      },
+      // 可选项 当名称与后台返回不一致时可添加该属性更改字段名
+      option: {
+        type: Object,
+        default: () => {}
+      }
+    },
+    computed: {
+      optonObj() {
+        return Object.assign({
+          goods_small_logo: 'goods_small_logo',
+          goods_name: 'goods_name',
+          goods_price: 'goods_price'
+        }, this.option)
+      }
+    },
+    data() {
+      return {
+        defaultImg: "https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png"
+      };
+    },
+    methods: {
+      // 价格格式转换
+      priceFormat(num) {
+        return num.toFixed(2)
+      },
+      // 跳转方法
+      handleClcik(item) {
+        this.$emit('handleToDetail', item)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.goods-item {
+  padding: 10px 5px;
+  display: flex;
+  border-bottom: 1px solid #f0f0f0;
+  .goods-left {
+    image {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+    margin-right: 5px;
+  }
+  .goods-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-title {
+      font-size: 13px;
+    }
+    .goods-info {
+      .goods-price {
+        font-size: 16px;
+        color: #C00000;
+      }
+    }
+  }
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,7 @@
 <template>
   <view>
-    Goods_list
+    <!-- 调用商品的列表组件 -->
+    <goods-item :goodsList="goodsList" @handleToDetail="handleToDetail"></goods-item>
   </view>
 </template>
 
@@ -8,8 +9,58 @@
   export default {
     data() {
       return {
-        
+        queryObj: {
+          query: '', // 查询关键字
+          cid: '', // 商品分类id
+          pagesize: 10,
+          pagenum: 1
+        },
+        goodsList: [],
+        total: 0,
+        // 节流阀
+        isLoading: false
       };
+    },
+    onLoad(option) {
+      this.queryObj.query = option.query || ''
+      this.queryObj.cid = option.cid || ''
+      this.getGoodsList()
+    },
+    methods: {
+      async getGoodsList(cb) {
+        this.isLoading = true
+        const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        this.isLoading = false
+        // 下拉刷新调用callback回调
+        cb && cb()
+        if(res.meta.status !== 200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      // 跳转详情调用
+      handleToDetail(item) {
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+        })
+      }
+    },
+    // 上拉加载，触底触发方法
+    onReachBottom() {
+      // 当pageNum * pageSize 大于等于总数时代表没有更多了
+      if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('没有更多数据了!')
+      if(this.isLoading) return
+      this.queryObj.pagenum += 1
+      this.getGoodsList()
+    },
+    // 下拉刷新
+    onPullDownRefresh() {
+      // 重置数据
+      this.queryObj.pagenum = 1
+      this.queryObj.isLoading = false
+      this.total = 0
+      this.goodsList = []
+      // 请求数据
+      this.getGoodsList(() => uni.stopPullDownRefresh())
     }
   }
 </script>
